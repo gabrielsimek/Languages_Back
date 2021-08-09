d290ea34cfc304b9d37c3ae33f2ac99a86ad4fa4
Gabriel Simek <gabrielsimek@outlook.com>
2021-08-09 15:41:44 -0700

    add User model and relationship

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 import app from './lib/app.js';
 import db from './lib/utils/db.js';
-
+import User from './lib/models/User.js';
+import Languages from './lib/controllers/languages.js';
+import './lib/models/index.js';
 const PORT = process.env.PORT || 7890;
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Started on ${PORT}`);
+  db.sync({ force: true });
 });
 
 process.on('exit', () => {
